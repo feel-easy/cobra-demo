d59d7d3a35f4254965b07f470a2540c76fab483b
l.wang <l.wang@rccchina.com>
2022-02-28 19:28:28 +0800

    排序

--- demo/slice.go
+++ demo/slice.go
@@ -22,5 +22,44 @@ func SliceDemo() {
 		cap  int
 	}{uintptr(ptr), 10, 10}
 	s := *(*[]byte)(unsafe.Pointer(&s1))
-	fmt.Println(len(s))
+	s3 := arr[:3]
+	s3 = append(s3, 100)
+	fmt.Println(len(s), s3, arr)
+	arr3 := []int{1, 9, 10, 30, 2, 5, 45, 8, 63, 234, 12}
+	fmt.Println(QuickSort(arr3))
+}
+
+func QuickSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+	splitdata := arr[0]          //第一个数据
+	low := make([]int, 0, 0)     //比我小的数据
+	hight := make([]int, 0, 0)   //比我大的数据
+	mid := make([]int, 0, 0)     //与我一样大的数据
+	mid = append(mid, splitdata) //加入一个
+	for i := 1; i < len(arr); i++ {
+		if arr[i] < splitdata {
+			low = append(low, arr[i])
+		} else if arr[i] > splitdata {
+			hight = append(hight, arr[i])
+		} else {
+			mid = append(mid, arr[i])
+		}
+	}
+	low, hight = QuickSort(low), QuickSort(hight)
+	myarr := append(append(low, mid...), hight...)
+	return myarr
+}
+
+//冒泡排序
+func BubbleSort(arr []int) []int {
+	for i := 0; i < len(arr); i++ {
+		for j := i + 1; j < len(arr); j++ {
+			if arr[i] > arr[j] {
+				arr[i], arr[j] = arr[j], arr[i]
+			}
+		}
+	}
+	return arr
 }
